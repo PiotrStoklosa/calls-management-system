bff2c9efb0630a4d39dba95e8de6461a3ed40f92
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-04-21 20:33:06 +0200

    Initial commit

---
+++ src/main/java/com/nokia/uwr/CallsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nokia.uwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallsManagementSystemApplication.class, args);
+	}
+
+}
