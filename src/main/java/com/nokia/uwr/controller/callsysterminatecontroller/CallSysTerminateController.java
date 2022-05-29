50c3f25c33c467a1a09323b539ac9be43b9490db
BM-ska <barbaramoczulska1a@gmail.com>
2022-05-29 12:52:59 +0200

    Add terminate call integration test and terminate controller

---
+++ src/main/java/com/nokia/uwr/controller/callsysterminatecontroller/CallSysTerminateController.java
@@ -0,0 +1,28 @@
+package com.nokia.uwr.controller.callsysterminatecontroller;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller to terminate CallSys.
+ *
+ * @author Barbara Moczulska
+ */
+@RestController
+@RequestMapping("/api/terminate")
+@RequiredArgsConstructor
+public class CallSysTerminateController {
+    private static final Logger LOGGER = LogManager.getLogger(CallSysTerminateController.class);
+
+    /**
+     * Terminate CallSys
+     */
+    @PostMapping
+    public void terminate() {
+        LOGGER.info("Terminated simulation");
+    }
+}
--- src/test/java/com/nokia/uwr/controller/callscontroller/CallsControllerIntegrationTest.java
+++ src/test/java/com/nokia/uwr/controller/callscontroller/CallsControllerIntegrationTest.java
@@ -18,6 +18,7 @@ class CallsControllerIntegrationTest {
     private static final String INITIALIZE_URL = "/api/initializer";
     private static final String START_CALL_URL = "/api/calls/start";
     private static final String STOP_CALL_URL = "/api/calls/stop";
+    private static final String TERMINATE_URL = "/api/terminate";
 
     private static final String BTS_LIST = """
             [   {
@@ -130,6 +131,35 @@ class CallsControllerIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(MEASUREMENTS))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Call startCall method which should not throw exception while given measurements aren't empty" +
+            "and CallSys is initialized" +
+            "and call stopCall method" +
+            "and CallSys is terminated")
+    public void test5() throws Exception {
+        restBTSMockMvc
+                .perform(post(INITIALIZE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(BTS_LIST))
+                .andExpect(status().isOk());
+
+        restBTSMockMvc
+                .perform(post(START_CALL_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(MEASUREMENTS))
+                .andExpect(status().isOk());
+
+        restBTSMockMvc
+                .perform(post(STOP_CALL_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(MEASUREMENTS))
+                .andExpect(status().isOk());
+
+//        restBTSMockMvc
+//                .perform(post(TERMINATE_URL))
+//                    .andExpect(status().isOk());
     }
 
 }
